f4a5e83042a7fc2df14fcc9a696af8b4ad20ee12
karim <karim.ali>
2021-05-26 23:33:24 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  appId: 'a177f8481c31fa96c3f95ad4f4f84610',
+  username: 'localcast',
+  baseUrl: 'https://api.darksky.net/forecast/',
+  geonamesApi: 'api',
+}
